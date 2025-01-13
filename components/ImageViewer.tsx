2ceef1293ecbe47b79a8a24e6d08394c6edef473
yooouri <pswja06@naver.com>
2025-01-13 19:13:43 +0900

    Use an image picker

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -2,18 +2,19 @@ import { StyleSheet } from "react-native";
 import { Image, type ImageSource } from "expo-image";
 
 type Props = {
-    imageSource: ImageSource
+  imgSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({imageSource}: Props) {
-    return <Image source={imageSource} style={styles.image} />
-
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 320,
-        height: 440,
-        borderRadius: 18,
-    }
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
 });
